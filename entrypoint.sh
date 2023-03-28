33d8e9c84c329918cb5d645ae0e1b5da0883766c
HerrSammy <38103916+HerrSammyDE@users.noreply.github.com>
2023-03-28 03:10:29 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ "${1#-}" != "$1" ]; then
-  set -- php-fpm "$@"
-fi
-
+# Führe die folgenden Befehle aus, wenn das erste Argument "php-fpm" oder "php" ist
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ]; then
-
   # Cache everything
   php artisan optimize
   php artisan config:cache
   php artisan route:cache
   php artisan view:cache
   php artisan event:cache
-#  php artisan ziggy:generate
-
 fi
 
+# Starte die erforderlichen Dienste im Hintergrund
 exec "$@" &
-exec nginx
-exec crond
+exec nginx &
+exec crond -f
